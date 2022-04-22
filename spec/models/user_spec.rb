22d9883c46099897f580d45abf47652d45db57cc
Victor Basantes <victor_basantes@hotmail.com>
2022-04-22 06:41:25 -0500

    Modify test for user_spec to have parenthesis for practice

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe "create" do
+  describe ("create") do
 
     before do
       @user = User.create(email: "usertest@test.com", password: "asdasd", password_confirmation: "asdasd", first_name: "John", last_name: "Snow")
     end
 
-    it "can be created" do
-      expect(@user).to be_valid
+    it ("can be created") do
+      expect(@user).to(be_valid())
     end
 
-    it "cannot be created without first_name, last_name" do
+    it ("cannot be created without first_name, last_name") do
       @user.first_name = nil
       @user.last_name = nil
-      expect(@user).to_not be_valid
+      expect(@user).to_not(be_valid())
     end
 
   end
