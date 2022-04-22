0b6e7320e7732f7b845a037d618391382711db19
Victor Basantes <victor_basantes@hotmail.com>
2022-04-22 10:34:12 -0500

    Add post functionality with BDD and TDD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    @post.save
+    # We don't want a create template, we want to render a show template
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,4 +14,19 @@ describe 'navigate' do
     end
   end
 
+  describe 'creation' do
+    it "has a new form that can be reached" do
+      visit (new_post_path)
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be created from new form page" do
+      visit (new_post_path)
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+      expect(page).to have_content("Some rationale")
+    end
+  end
+
 end
